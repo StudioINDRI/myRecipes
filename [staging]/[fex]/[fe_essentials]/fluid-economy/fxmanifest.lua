4ab9f5b5d19d425a32045fae9c9f7a1c647b6a10
Bianco Francesco <42351006+StudioINDRI@users.noreply.github.com>
2022-11-09 15:04:32 +0100

    adding framework

--- [staging]/[fex]/[fe_essentials]/fluid-economy/fxmanifest.lua
+++ [staging]/[fex]/[fe_essentials]/fluid-economy/fxmanifest.lua
@@ -1,9 +1,46 @@
 fx_version 'bodacious'
 game 'gta5'
 
-author 'Francesco Bianco - github.com/studioindri)'
-version '1.0.0'
+author 'Francesco Bianco - github.com/studioindri'
+version '0.1.0'
 
-client_script 'client.lua'
-server_script 'server.lua'
-shared_script 'shared.lua'
+
+client_scripts {
+  -- NATIVE UI DEPENDENCY
+  -- COMMENTA SE NON USI NATIVE UI
+  --'@NativeUILua_Reloaded/src/NativeUIReloaded.lua',
+
+  -- SOURCE
+  'config.lua',   
+  'utils.lua',
+  'client/main.lua',
+  'client/framework/framework_functions.lua'
+}
+
+server_scripts {
+  -- MYSQL DEPENDENCY
+  --'@mysql-async/lib/MySQL.lua',
+  '@oxmysql/lib/MySQL.lua',
+
+  -- SOURCE
+  'config.lua',
+  'credentials.lua',  
+  'server/framework/framework_functions.lua',
+  'server/functions.lua',
+  'server/main.lua',
+}
+
+dependencies {
+  -- COMMENTA SE NON USI ESX
+  'es_extended',
+
+  -- DECOMMENTA SE NON USI NATIVEUI
+  --'NativeUILua_Reloaded',
+
+  --segli mysql in uso:
+  'oxmysql',
+  --'mysql-async'
+
+  --commenta se non usi ox_libs
+  'ox_libs',
+}
