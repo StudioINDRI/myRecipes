6554496420f79ec3fd86d047cef754410759c992
Bianco Francesco <42351006+StudioINDRI@users.noreply.github.com>
2022-11-09 14:21:35 +0100

    creating [fex]

---
+++ [staging]/[fex]/[fe_essentials]/[fe_aicore]/npccrew/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "cerulean"
+games {"gta5"}
+
+name "npcspawner"
+author "Francesco Bianco - github.com/StudioINDRI"
+version "1.0"
+
+client_scripts {
+    'cl_main.lua',
+    'config.lua'
+}
+
+server_scripts {
+	'sv_main.lua',
+}
